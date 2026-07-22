598830e7895678ca090fc6c4c350ac5d36d0bc7e
SUNNY KUMAR <sandykumar800234@gmail.com>
2026-07-23 03:01:24 +0530

    Added dashboard and login validation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
-    <div>
-      <Login />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,23 +1,56 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "../styles/Login.css";
 
 function Login() {
+  const navigate = useNavigate();
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  function handleLogin() {
+    console.log("Username:", `"${username}"`);
+    console.log("Password:", `"${password}"`);
+
+    if (username.trim() === "admin" && password.trim() === "1234") {
+      setError("");
+      navigate("/dashboard");
+    } else {
+      setError("Invalid username or password");
+    }
+  }
+
   return (
     <div className="login-container">
       <div className="login-card">
         <h1 style={{ color: "white", fontSize: "50px" }}>Mini Smart ERM</h1>
+
         <p>Please login to continue</p>
 
         <div className="input-group">
           <label>Username</label>
-          <input type="text" placeholder="Enter your username" />
+          <input
+            type="text"
+            placeholder="Enter your username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
         </div>
 
         <div className="input-group">
           <label>Password</label>
-          <input type="password" placeholder="Enter your password" />
+          <input
+            type="password"
+            placeholder="Enter your password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </div>
 
-        <button>Login</button>
+        {error && <p className="error-message">{error}</p>}
+
+        <button onClick={handleLogin}>Login</button>
       </div>
     </div>
   );
